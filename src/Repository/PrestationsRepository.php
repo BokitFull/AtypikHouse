652b9a3686430510d923022daa72b1bb42a50a20
Axel <c0l0r2aliza@gmail.com>
2022-07-17 20:48:28 +0200

    Add repository

--- src/Repository/PrestationsRepository.php
+++ src/Repository/PrestationsRepository.php
@@ -2,26 +2,26 @@
 
 namespace App\Repository;
 
-use App\Entity\Promotions;
+use App\Entity\Prestations;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Promotions>
+ * @extends ServiceEntityRepository<Prestations>
  *
- * @method Promotions|null find($id, $lockMode = null, $lockVersion = null)
- * @method Promotions|null findOneBy(array $criteria, array $orderBy = null)
- * @method Promotions[]    findAll()
- * @method Promotions[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Prestations|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Prestations|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Prestations[]    findAll()
+ * @method Prestations[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class PromotionsRepository extends ServiceEntityRepository
+class PrestationsRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Promotions::class);
+        parent::__construct($registry, Prestations::class);
     }
 
-    public function add(Promotions $entity, bool $flush = false): void
+    public function add(Prestations $entity, bool $flush = false): void
     {
         $this->getEntityManager()->persist($entity);
 
@@ -30,7 +30,7 @@ class PromotionsRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Promotions $entity, bool $flush = false): void
+    public function remove(Prestations $entity, bool $flush = false): void
     {
         $this->getEntityManager()->remove($entity);
 
@@ -40,7 +40,7 @@ class PromotionsRepository extends ServiceEntityRepository
     }
 
 //    /**
-//     * @return Promotions[] Returns an array of Promotions objects
+//     * @return Prestations[] Returns an array of Prestations objects
 //     */
 //    public function findByExampleField($value): array
 //    {
@@ -54,7 +54,7 @@ class PromotionsRepository extends ServiceEntityRepository
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?Promotions
+//    public function findOneBySomeField($value): ?Prestations
 //    {
 //        return $this->createQueryBuilder('p')
 //            ->andWhere('p.exampleField = :val')
