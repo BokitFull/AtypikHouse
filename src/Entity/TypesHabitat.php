cbad4aeec0318e85109c90510dbce6218c5885b0
Axel <c0l0r2aliza@gmail.com>
2022-07-26 11:56:23 +0200

    Add flash message, fix user roles swithcing, add auto created and updated in entity, add history on reservations table

--- src/Entity/TypesHabitat.php
+++ src/Entity/TypesHabitat.php
@@ -8,9 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-
 #[ORM\Entity(repositoryClass: TypesHabitatRepository::class)]
-#[Gedmo\Loggable]
 class TypesHabitat
 {
     #[ORM\Id]
@@ -18,16 +16,17 @@ class TypesHabitat
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[Gedmo\Versioned]
     #[ORM\Column(type: 'string', length: 50)]
     private $nom;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $description;
 
+    #[Gedmo\Timestampable(on: 'create')]
     #[ORM\Column(type: 'datetime_immutable')]
     private $created_at;
 
+    #[Gedmo\Timestampable(on: 'update')]
     #[ORM\Column(type: 'datetime_immutable', nullable: true)]
     private $updated_at;
 
